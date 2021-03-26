ff75768fa0d684b4b3e258c37f243f7fbfa8d552
Sveta <bogatovasvetlanau@gmail.com>
2021-03-26 20:34:28 +0200

    Initial commit

--- Practice3/Program.cs
+++ Practice3/Program.cs
@@ -1,15 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Practice3
 {
     class Program
     {
+        private static int Lower { get; set; }
+        private static int Upper { get; set; }
         static void Main(string[] args)
         {
+            Console.WriteLine("Set the lower value of the range!");
+            Lower = Validator.GetValidatedInput();
+
+            Console.WriteLine("Set the upper value of the range!");
+            Upper = Validator.GetValidatedInput();
+
+            if (IsRangeGoodToCreateArray(Lower, Upper))
+            {
+                int[] newArray = (int[])GetArrayOfNaturalNumbers(Lower, Upper);
+                GetSumOfNumbersDevidableOn3AndNotDevidableOn5(newArray);
+            }
+            else
+            {
+                Console.WriteLine("Less then 11 values range, exit");
+            }
+             Console.ReadKey();
+        }
+
+        private static bool IsRangeGoodToCreateArray(int lowere, int uppere)
+        {
+            return ((Upper - Lower + 1) > 10);
+        }
+
+        private static Array GetArrayOfNaturalNumbers(int lower, int upper)
+        {
+            int[] numbersArray = new int[Upper - Lower + 1];
+            int i = 0;
+            do
+            {
+               numbersArray[i] = Lower + i;
+               Console.WriteLine("Array element with i={0} is {1}", i, numbersArray[i]);
+               i++;
+            }
+            while ((Lower + i <= Upper) && (i <= Upper - Lower));
+            return numbersArray;
+        }
+
+        private static void GetSumOfNumbersDevidableOn3AndNotDevidableOn5(int [] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < Upper - Lower; i++)
+            {
+                if ((array[i] % 3 == 0) && (array[i] % 5 != 0))
+                {
+                    sum = sum + array[i];
+                }
+            }
+            Console.WriteLine("Sum of elements devidable on 3 and not devidable on 5 is : " + sum);
         }
     }
 }
---
+++ Practice3/Validator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Practice3
+{
+    public static class Validator
+    {
+        public static int Border { get; set; }
+        public const int MAX_ATTEMPTS = 10;
+
+        public static int GetValidatedInput()
+        {
+            Console.WriteLine("\nWhat is the Value, should be more than 0");
+            int attempt = 0;
+            while (attempt < MAX_ATTEMPTS)
+            {
+                string inputString = Console.ReadLine();
+                if (inputString.ToString() != string.Empty)
+                {
+                    try
+                    {
+                        if ((inputString.IndexOf('0') != -1) && (inputString.Length == 1))
+                        {
+                            Console.WriteLine("\nIncorrect Input: Zero is not allowed");
+                            attempt++;
+                            continue;
+                        }
+                        else
+                        {
+                            Border = Convert.ToInt32(inputString);
+                            Console.WriteLine("\nBorder is " + Border);
+                            return Border;
+                        }
+                    }
+                    catch (SystemException ex)
+                    {
+                        Console.WriteLine("\nIncorrect Input, error" + ex.Message);
+                        Console.WriteLine("\nTry again to populate correct border");
+                        attempt++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nIncorrect Input: Empty value is not allowed");
+                }
+            }
+                return 0;
+        }
+    }
+}
