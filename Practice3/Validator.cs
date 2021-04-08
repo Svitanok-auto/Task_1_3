adba34061c6deca71335d2550bdb7963773c040b
Sveta <bogatovasvetlanau@gmail.com>
2021-04-08 19:21:43 +0300

    Updates are: 1 Validation for negative input is added; 2 Removed Border property and added local variable border;

--- Practice3/Validator.cs
+++ Practice3/Validator.cs
@@ -5,12 +5,12 @@ namespace Practice3
     public static class Validator
     {
         public const int MAX_ATTEMPTS = 10;
-        public static int Border { get; set; }
 
         public static int GetValidatedInput()
         {
             Console.WriteLine("What is the Value, should be more than 0");
             int attempt = 0;
+            int border = 0;
             while (attempt < MAX_ATTEMPTS)
             {
                 string inputString = Console.ReadLine();
@@ -24,11 +24,17 @@ namespace Practice3
                             attempt++;
                             continue;
                         }
+                        else if (Convert.ToInt32(inputString) < 0)
+                        {
+                            Console.WriteLine("\nIncorrect Input: Negative values are not allowed");
+                            attempt++;
+                            continue;
+                        }
                         else
                         {
-                            Border = Convert.ToInt32(inputString);
-                            Console.WriteLine("\nBorder is " + Border);
-                            return Border;
+                            border = Convert.ToInt32(inputString);
+                            Console.WriteLine("\nBorder is " + border);
+                            return border;
                         }
                     }
                     catch (SystemException ex)
